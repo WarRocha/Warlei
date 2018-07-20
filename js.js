0a86043bdecf16e003f3be86598a528de02728bc
Warlei Rocha <frontwarlei@gmail.com>
2018-07-20 09:04:05 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+ jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){        
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500 );});
+            });
+
+
+    function menu() {
+            var  pegarMenu = document.getElementById("menu");
+            if (pegarMenu.className === "nav") {
+            pegarMenu.className += " responsivo";
+            } else {
+            pegarMenu.className = "nav";
+            }}
